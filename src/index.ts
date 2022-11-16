25a41c2da0041c98c1d36d65ec09418692911fb4
Muhammed Eroğlu <llofmejb@gmail.com>
2022-11-16 17:27:39 +0300

    ts start

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// npm install -g typescript
+// npm init --y (package.json içeriği)
+// +tsconfig.json (ts buna göre çalışır)
+// ts -> je'e tip desteği sağlar
+// npm install @types/node  (nodejs tipleri)
+
+// @ts-ignore    (ALT SATIRDAKİ HATAYI GÖRME)
+// a=asdasd
+
+console.log("hello");
+
