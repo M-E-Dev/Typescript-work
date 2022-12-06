e8cc4aa26d09605098d72c29a7fc10b62ea4d062
Muhammed Eroğlu <llofmejb@gmail.com>
2022-12-06 17:02:27 +0300

    TS lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
 import './App.css';
+import Button from './components/Button';
 import Greet from './components/Greet';
+import Heading from './components/Heading';
+import Input from './components/Input';
+import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
+import Status from './components/Status';
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne"
-  }
-
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne"
-    },
-    {
-      first: "Clark",
-      last: "Kent"
-    },
-    {
-      first: "Harry",
-      last: "Potter"
-    }
-  ]
-
-
+  // const personName = {
+  //   first: "Bruce",
+  //   last: "Wayne"
+  // }
+  // const nameList = [
+  //   {
+  //     first: "Bruce",
+  //     last: "Wayne"
+  //   },
+  //   {
+  //     first: "Clark",
+  //     last: "Kent"
+  //   },
+  //   {
+  //     first: "Harry",
+  //     last: "Potter"
+  //   }
+  // ]
   return (
     <div className="App">
-      <Greet name="Harry" messageCount={20} isLoggedIn={false} />
+      {/* <Greet name="Harry" messageCount={20} isLoggedIn={false} />
       <Person name={personName}/>
-      <PersonList names={nameList}/>
+      <PersonList names={nameList}/> */}
+      {/* --------------------------------------- */}
+      {/* <Status status="success" />
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name="Harry" isLoggedIn={false} /> */}
+      {/* --------------------------------------- */}
+      {/* <Button
+        handleClick={(event, id) => {
+          console.log("Button Clicked", event, id)
+        }} /> */}
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
-
 export default App;
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,11 @@
 type GreetProps = {
     name: string,
-    messageCount: number,
+    messageCount?: number,
     isLoggedIn: boolean,
 }
 
 const Greet = (props: GreetProps) => {
+    const { messageCount = 0 } = props
     return (
         <div>
             {
