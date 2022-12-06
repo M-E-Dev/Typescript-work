55c39a9a52534bca9aa6069f667c557982e0e301
Muhammed Eroğlu <llofmejb@gmail.com>
2022-12-06 15:15:31 +0300

    lesson1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne"
+  }
+
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne"
+    },
+    {
+      first: "Clark",
+      last: "Kent"
+    },
+    {
+      first: "Harry",
+      last: "Potter"
+    }
+  ]
+
+
   return (
     <div className="App">
-      APP
+      <Greet name="Harry" messageCount={20} isLoggedIn={false} />
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,17 @@
+type GreetProps = {
+    name: string,
+    messageCount: number,
+    isLoggedIn: boolean,
+}
+
+const Greet = (props: GreetProps) => {
+    return (
+        <div>
+            {
+             props.isLoggedIn ? <h1>Welcome {props.name}, You have {props.messageCount} unread messages...</h1> : <h1>Welcome Guest</h1>
+            }
+        </div>
+    )
+}
+
+export default Greet
